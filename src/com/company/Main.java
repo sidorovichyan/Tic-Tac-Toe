251762420969c5936874c746b2e1ab41781773b9
YanSidorovich <sidorovich.yan@bk.ru>
2018-12-20 22:28:03 +0300

    test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Check");
         System.out.println("chec");
-        System.out.println("LEXA LO     .!.  (.)(.)    бмл");
+        System.out.println("  бмл");
 
 
 
