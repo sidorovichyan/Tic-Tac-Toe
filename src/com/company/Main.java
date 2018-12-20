24a4c1f6acff8c43267d465986bbbf46ccf26c38
guerrk1n <aleksej684549@inbox.ru>
2018-12-20 22:42:49 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,12 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Check");
-        System.out.println("chec");
-        System.out.println("  бл");
-
-
-
-
+        
+        
 
     }
 }
