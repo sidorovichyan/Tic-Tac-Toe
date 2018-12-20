d5fde2135258b4ac5a18edca1c6f7ab552a940dc
guerrk1n <aleksej684549@inbox.ru>
2018-12-20 22:38:40 +0300

    test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Check");
         System.out.println("chec");
-        System.out.println("  бмл");
+        System.out.println("  бл");
 
 
 
