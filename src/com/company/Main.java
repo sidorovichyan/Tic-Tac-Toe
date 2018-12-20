6dc4b7ab6878b9194ef0ca4fbd0ce2acf1c51a8b
YanSidorovich <sidorovich.yan@bk.ru>
2018-12-20 22:23:13 +0300

    test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Check");
         System.out.println("chec");
+        System.out.println("LEXA LO     .!.  (.)(.)    бмл");
+
+
+
+
+
     }
 }
